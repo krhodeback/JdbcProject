358df94983fba300fe2be3cc0291a3b3e33f40d6
Yefim Savin <savin.efim2@gmail.com>
2021-07-15 23:52:12 +0500

    convert dao to hibernate

--- src/main/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,41 +1,69 @@
 package jm.task.core.jdbc.dao;
 
+import java.util.EnumSet;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.schema.TargetType;
+
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private Util util;
+    private EnumSet<TargetType> enumSet;
 
+    public UserDaoHibernateImpl() {
+        util = new Util();
+        enumSet = EnumSet.of(TargetType.DATABASE);
     }
 
     @Override
     public void createUsersTable() {
-
+        new SchemaExport().create(enumSet, util.getMetadata());
     }
 
     @Override
     public void dropUsersTable() {
-
+        new SchemaExport().drop(enumSet, util.getMetadata());
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = util.getSession();
+        session.save(new User(name, lastName, age));
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = util.getSession();
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+        User user = (User) session.load(User.class, id);
+        if (user != null) {
+            session.delete(user);
+        }
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = util.getSession();
+        List<User> userList = session.createQuery("SELECT a FROM User a", User.class).getResultList();
+        session.close();
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        List<User> userList = getAllUsers();
+        for (User user : userList) {
+            removeUserById(user.getId());
+        }
 
     }
 }
